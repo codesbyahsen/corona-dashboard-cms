3e8eb7a2aa85bd8da544328318be0fd97e4f8770
Ahsan Rehmani <rehmaniahsan15@gmail.com>
2023-02-24 10:01:20 +0500

    Added summernote editor in these modules [FAQs, Newsletter, Privacy Policies, Terms and Conditions]

--- resources/views/modules/dashboard/index.blade.php
+++ resources/views/modules/dashboard/index.blade.php
@@ -5,31 +5,6 @@
 @section('page-content')
     <div class="content-wrapper">
         <div class="row">
-            <div class="col-12 grid-margin stretch-card">
-                <div class="card corona-gradient-card">
-                    <div class="card-body py-0 px-0 px-sm-3">
-                        <div class="row align-items-center">
-                            <div class="col-4 col-sm-3 col-xl-2">
-                                <img src="assets/images/dashboard/Group126@2x.png" class="gradient-corona-img img-fluid"
-                                    alt="">
-                            </div>
-                            <div class="col-5 col-sm-7 col-xl-8 p-0">
-                                <h4 class="mb-1 mb-sm-0">Want even more features?</h4>
-                                <p class="mb-0 font-weight-normal d-none d-sm-block">Check out our Pro version with 5 unique
-                                    layouts!</p>
-                            </div>
-                            <div class="col-3 col-sm-2 col-xl-2 pl-0 text-center">
-                                <span>
-                                    <a href="https://www.bootstrapdash.com/product/corona-admin-template/" target="_blank"
-                                        class="btn btn-outline-light btn-rounded get-started-btn">Upgrade to PRO</a>
-                                </span>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div class="row">
             <div class="col-xl-3 col-sm-6 grid-margin stretch-card">
                 <div class="card">
                     <div class="card-body">
