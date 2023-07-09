9e1dcb7f8b00795a22c7f841e31e2df5db6047b8
AHSEN ALEE <ahsenalee4@gmail.com>
2023-07-09 11:36:50 +0500

    Added assets files, theme partials, layout, and dashboard page.

---
+++ resources/views/admin/layout/app.blade.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta name="X-CSRF-TOKEN" content="{{ csrf_token() }}" />
+    <title>@yield('title') - {{ config('app.name', 'Corona Dashboard') }}</title>
+
+    @include('admin.partials.links')
+</head>
+
+<body>
+    <div class="container-scroller">
+        @include('admin.partials.sidebar-nav')
+
+        <div class="container-fluid page-body-wrapper">
+            @include('admin.partials.header-nav')
+
+            <div class="main-panel">
+                @yield('content')
+
+                @include('admin.partials.footer')
+            </div>
+        </div>
+    </div>
+
+    @include('admin.partials.scripts')
+</body>
+
+</html>
---
+++ resources/views/admin/partials/footer.blade.php
@@ -0,0 +1,8 @@
+<footer class="footer">
+    <div class="d-sm-flex justify-content-center justify-content-sm-between">
+        <span class="text-muted d-block text-center text-sm-left d-sm-inline-block">&copy;
+            <script>document.write(new Date().getFullYear());</script>
+        </span>
+      <span class="float-none float-sm-right d-block mt-1 mt-sm-0 text-center"> Free <a href="https://github.com/codesbyahsen/laravel-general-cms" target="_blank">Corona Admin Dashboard</a> by Ahsen Alee</span>
+    </div>
+</footer>
