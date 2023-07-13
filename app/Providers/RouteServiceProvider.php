71711e1daa072758f42acb88d2f7ee74f7a11e53
AHSEN ALEE <ahsenalee4@gmail.com>
2023-07-14 00:24:23 +0500

    Replace predefined auth pages with theme auth pages.

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -18,6 +18,7 @@ class RouteServiceProvider extends ServiceProvider
      * @var string
      */
     public const HOME = '/dashboard';
+    public const ADMIN_HOME = '/admin/dashboard';
 
     /**
      * Define your route model bindings, pattern filters, and other route configuration.
